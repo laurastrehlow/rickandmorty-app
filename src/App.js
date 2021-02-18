b33890c51277aec78f07a7c7d35096b81762820c
Laura Strehlow <laurastrehlow09@aol.com>
2021-02-18 15:42:09 +0100

    Add all characters

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import {results} from './rickandmortyapi.json'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card'
 import './App.css'
 
 function App() { 
   const [userInput, setUserInput] = useState('')
+  const [characters, setCharacters] = useState([])
+  
+  useEffect(() => {
+    getAllCharacters()
+  }) 
+
+    function getAllCharacters(url = 'https://rickandmortyapi.com/api/character') {
+    
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {setCharacters(oldState => [...oldState, ...data.results])
+      
+          const nextUrl = data.info.next
+          nextUrl && getAllCharacters(nextUrl)
+        })
+      }
+
   return <>
   <input 
         value={userInput}
         onChange={event => setUserInput(event.target.value)} placeholder="Search character..." />
          {
-          results.filter(character => character.name.toLowerCase().includes(userInput.toLowerCase()))
+          characters.filter(character => character.name.toLowerCase().includes(userInput.toLowerCase()))
           .map(character => <Card key={character.id} name={character.name} species={character.species} image={character.image} status={character.status} location={character.location.name} origin={character.origin.name}/>)}
         </>
 }
