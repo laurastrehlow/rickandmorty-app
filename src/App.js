ce62080e0d58f917304b51cf02fc209604268b93
Laura Strehlow <laurastrehlow09@aol.com>
2021-02-18 14:58:15 +0100

    Add nameFilter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div className="App">
- Hello world
-    </div>
-  );
+import {results} from './rickandmortyapi.json'
+import React, { useState } from 'react';
+import Card from './Card'
+import './App.css'
+
+function App() { 
+  const [userInput, setUserInput] = useState('')
+  return <>
+  <input 
+        value={userInput}
+        onChange={event => setUserInput(event.target.value)} placeholder="Search character..." />
+         {
+          results.filter(character => character.name.toLowerCase().includes(userInput.toLowerCase()))
+          .map(character => <Card key={character.id} name={character.name} species={character.species} image={character.image} status={character.status} location={character.location.name} origin={character.origin.name}/>)}
+        </>
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import './Card.css'
+import React, { useState } from 'react';
+function Card({ name, species, image, status, location, origin }) {
+    const [isTextVisible, setIsTextVisible] = useState(false)
+
+    return <section 
+             className="Card">
+             <img src={image} alt="" height="200"/>
+      <h2>{name}</h2>
+      <button onClick={event => {
+          event.stopPropagation()
+          setIsTextVisible(!isTextVisible)
+        }}>{isTextVisible ? 'Hide details' : 'Show details'}</button>
+      <p hidden={!isTextVisible}>Species: {species}<br/>
+      Status: {status}<br/>
+      Location: {location}<br/>
+      Origin: {origin}</p>
+    </section>
+  }
+
+  export default Card;
